3056a706ffe767a0eda9c1aec5c1143cc041f305
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-20 23:31:34 +0300

    addScene2

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SceneLoader : MonoBehaviour
+    {
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Debug.Log("1");
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
