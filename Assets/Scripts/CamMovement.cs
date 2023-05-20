062fbd15de72c30d3eba86e822d951b3f592e71e
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-20 21:13:40 +0300

    addLimitForCamera

--- Assets/Scripts/CamMovement.cs
+++ Assets/Scripts/CamMovement.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    public class CamMovement : MonoBehaviour
+public class CamMovement : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField]private float LeftLimit;
+    [SerializeField] private float RightLimit;
+    [SerializeField] private float UpLimit;
+    [SerializeField] private float DownLimit;
+
+    void LateUpdate()
     {
-        public GameObject player;
-        void LateUpdate()
-        {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, -10f);
-        }
-
-
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, -10f);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x,LeftLimit,RightLimit), Mathf.Clamp(transform.position.y, DownLimit, UpLimit), transform.position.z);
     }
+
+
+}
