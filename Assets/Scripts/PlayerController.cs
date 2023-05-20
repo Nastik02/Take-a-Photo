24b6291737fbdec3a2f8488a71d8eb301c547237
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-20 14:34:07 +0300

    add_movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 5f;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        sprite.flipX = Input.GetAxis("Horizontal") < 0.0f;
+    }
+    void FixedUpdate()
+    {
+        Movement();
         
     }
+    private void Movement()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+    }
+
+
 }
