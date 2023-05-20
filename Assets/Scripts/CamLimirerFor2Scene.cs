11a45a8c0a820116a1d79af3bcce7dcdce4b4853
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-21 02:34:05 +0300

    addlimforcam

---
+++ Assets/Scripts/CamLimirerFor2Scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CamLimirerFor2Scene : MonoBehaviour
+    {
+        private float LeftLimit= 32.8f;
+        private float RightLimit=33.5f;
+        private float UpLimit= -5;
+        private float DownLimit=-59;
+        void LateUpdate()
+        {
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, LeftLimit, RightLimit), Mathf.Clamp(transform.position.y, DownLimit, UpLimit), transform.position.z);
+        }
+
+    }
+}
