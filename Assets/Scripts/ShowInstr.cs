56685809e2f48ae95b89e4d4776edbe5e20df900
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-21 15:05:19 +0300

    add menud

---
+++ Assets/Scripts/ShowInstr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ShowInstr : MonoBehaviour
+    {
+
+        public GameObject arrow;
+        public Transform player;
+
+
+        
+        void Update()
+        {
+            if (arrow != null)
+            {
+                if (Vector2.Distance(transform.position, player.transform.position) < 3f)
+                {
+                    arrow.SetActive(true);
+                }
+                if (Vector2.Distance(transform.position, player.transform.position) > 3f)
+                {
+                    arrow.SetActive(false);
+                }
+            }
+
+
+        }
+     
+    }
+}
