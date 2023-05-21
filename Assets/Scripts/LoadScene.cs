01253b1402dc90911599f64c9fd1a3e14cb70990
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-21 17:08:11 +0300

    addfade

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts
+{
+    public class LoadScene : MonoBehaviour
+    {
+        public Animator anim;
+
+        
+        private void Start()
+        {
+            Debug.Log("7");
+            anim.SetTrigger("start");
+            
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ScreenFade : MonoBehaviour
+    {
+        private Animator anim;
+
+        // Use this for initialization
+        void Start()
+        {
+            anim= GetComponent<Animator>();
+            anim.SetBool("fade", false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
