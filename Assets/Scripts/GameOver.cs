a4a104e0b55b3f9764d9ace296fb416c67f4f496
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-21 18:24:38 +0300

    addgameover

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class GameOver : MonoBehaviour
+    {
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
--- Assets/Scripts/RedZone.cs
+++ Assets/Scripts/RedZone.cs
@@ -6,10 +6,12 @@ namespace Assets.Scripts
     public class RedZone : MonoBehaviour
     {
         public GameObject crch;
+        public AudioSource scream;
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
             {
+                scream.Play();
                 crch.SendMessage("Move");
             }
         }
