1afd6142ae96f9390f4a061b05ad8100fdc8a19d
Nastik02 <fyodorova.anastasya2018@yandex.ru>
2023-05-21 18:06:26 +0300

    addrestart

---
+++ Assets/Scripts/F3ScreenFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class F3ScreenFade : MonoBehaviour
+    {
+        private Animator anim;
+
+        // Use this for initialization
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            anim.SetBool("fade", false);
+        }
+        public void die()
+        {
+            StartCoroutine(DieCoroutine());
+        }
+        private IEnumerator DieCoroutine()
+        {
+            anim.SetBool("fade", true);
+            yield return new WaitForSeconds(1f);
+            anim.SetBool("fade", false);
+
+        }
+        
+    }
+}
